09fd7a1818d77bc04e658c47c42cbbbca4edbd01
TodMcChesney <codescribe.net@gmail.com>
2017-08-11 12:03:57 +0200

    feat: Add JS code to open/close modal

---
+++ src/js/scripts.js
@@ -0,0 +1,25 @@
+(() => {
+    'use strict';
+
+    const modal = document.getElementById('js-modal');
+    const openModalButtons = document.querySelectorAll('.js-open-modal');
+    const closeModalButton = document.querySelector('.js-close-modal');
+
+    openModalButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            modal.style.display = "block";
+            event.preventDefault();
+        }, false);
+    });
+
+    closeModalButton.addEventListener('click', (event) => {
+        modal.style.display = "none";
+        event.preventDefault();
+    }, false);
+
+    window.onclick = (event) => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+})();
