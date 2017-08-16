630c7aa7a057ced024290704b416ae9e4b5ba8eb
TodMcChesney <codescribe.net@gmail.com>
2017-08-16 15:18:56 +0200

    style: Remove unnecessary parentheses from ES6 JS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,19 +5,19 @@
     const openModalButtons = document.querySelectorAll('.js-open-modal');
     const closeModalButton = document.querySelector('.js-close-modal');
 
-    openModalButtons.forEach((button) => {
-        button.addEventListener('click', (event) => {
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', event => {
             modal.style.display = "block";
             event.preventDefault();
         }, false);
     });
 
-    closeModalButton.addEventListener('click', (event) => {
+    closeModalButton.addEventListener('click', event => {
         modal.style.display = "none";
         event.preventDefault();
     }, false);
 
-    window.onclick = (event) => {
+    window.onclick = event => {
         if (event.target === modal) {
             modal.style.display = "none";
         }
