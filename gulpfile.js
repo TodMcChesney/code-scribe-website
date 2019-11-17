c15a36a04e5023820f592c4a33d6c6ceff2a0c47
TodMcChesney <codescribe.net@gmail.com>
2019-11-17 12:47:34 -0500

    chore: Add Browsersync and default Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 /* jshint node: true */
 'use strict';
 
-const {src, dest} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-dart-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const autoprefixer = require ('autoprefixer');
+const browserSync = require('browser-sync').create();
 
-// Compile sass
+// Compile Sass
 function compileSass() {
     return src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
@@ -17,5 +18,29 @@ function compileSass() {
         .pipe(dest('src/css'));
 }
 
-// Export the default Gulp task so it can be run
-exports.default = compileSass;
+// Start Browsersync server
+function startServer(done) {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        },
+        browser: 'chrome'
+    });
+    done();
+}
+
+// Reload web page
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+// Watch for file changes and fire reload
+function watchChanges(done) {
+    watch('src/scss/**/*.scss', series(compileSass, reload));
+    watch(['src/*.html', 'src/js/*.js', 'src/img/*.+(png|jpg)'], reload);
+    done();
+}
+
+// Export the default Gulp task and assign dev tasks to be run in series
+exports.default = series(compileSass, startServer, watchChanges);
