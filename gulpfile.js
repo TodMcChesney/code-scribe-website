fcc34f8a9ad24bc7cde20dd48f74a46824c43176
TodMcChesney <codescribe.net@gmail.com>
2019-11-17 10:50:45 -0500

    chore: Add Dart Sass with Autoprefixer to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,21 @@
 /* jshint node: true */
 'use strict';
+
+const {src, dest} = require('gulp');
+const sass = require('gulp-dart-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require ('autoprefixer');
+
+// Compile sass
+function compileSass() {
+    return src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(dest('src/css'));
+}
+
+// Export the default Gulp task so it can be run
+exports.default = compileSass;
